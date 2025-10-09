129af8e70a46e12b4804fe2f93011d9679e3dc38
Lyuboslav Nalbantov <tails969@gmail.com>
2025-10-09 12:34:08 +0200

    Cutscene update
    audio added

--- Assets/_Scripts/MainMenu/CutsceneManager.cs
+++ Assets/_Scripts/MainMenu/CutsceneManager.cs
@@ -1,19 +1,19 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Collections;
 
 public class CutsceneManager : MonoBehaviour
 {
-
-    public void gameStart()
-    {
-        
-        //SceneManager.LoadScene("Lyubo");
-    }
+    [SerializeField]
+    AudioClip clip;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        StartCoroutine(PlayAudioAndLoadScene());
+        Debug.Log("Playing audio...");
+        //playAudio();
+        //SceneManager.LoadScene("Lyubo"); 
     }
 
     // Update is called once per frame
@@ -21,4 +21,23 @@ public class CutsceneManager : MonoBehaviour
     {
         
     }
+
+    private IEnumerator PlayAudioAndLoadScene()
+    {
+        yield return new WaitForSeconds(0.5f);
+
+        AudioSource source = GetComponent<AudioSource>();
+
+        if (source == null) source = GetComponent<AudioSource>();
+
+        source.clip = clip;
+        source.Play();
+
+        yield return new WaitForSeconds(clip.length);
+        yield return new WaitForSeconds(1f);
+
+        SceneManager.LoadScene("Lyubo");
+
+    }
+
 }
